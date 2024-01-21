6c062a5474959d183088f706baa64472c5306009
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-21 16:48:22 +0100

    setup jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <div className="message">
+        <p></p>
+      </div>
+      <div className="buttons">
+        <button>Previuos</button>
+        <button>Next</button>
+      </div>
     </div>
   );
 }
