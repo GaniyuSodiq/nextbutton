bc3c99851ebd92d9ea0b1f6e6e7f77454c3663cf
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-25 05:53:48 +0100

    create and use the step state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const messages = ["Learn React 📚", "Get Good Job 🤑", "Save Your Money 🏦"];
 
+  const [step, setStep] = useState(1);
+
+  function handleNext() {
+    step <= 3 && setStep(step + 1);
+  }
+
   return (
     <div className="App">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step >= 1 ? "numberColor" : ""}>1</div>
+        <div className={step >= 2 ? "numberColor" : ""}>2</div>
+        <div className={step >= 3 ? "numberColor" : ""}>3</div>
       </div>
       <div className="message">
         <p>{messages[0]}</p>
       </div>
       <div className="buttons">
         <button>Previuos</button>
-        <button>Next</button>
+        <button onClick={handleNext}>Next</button>
       </div>
     </div>
   );
